49255e56b0e5214d8a6dbf0705efcb046e6f9cec
PepVal <pepval001@gmail.com>
2020-07-13 14:16:48 -0500

    divide one component

--- src/components/search/Form.js
+++ src/components/search/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchButton from './SearchButton';
 
 class Form extends React.Component {
 
@@ -19,12 +20,9 @@ class Form extends React.Component {
     return (
       <form className="search-form"
        onSubmit={ e => this.props.handleSubmit(e, history, searchInput)}>
-        <button type="submit" className="search-button">
-          <svg className="svgcustom" height="32" width="32">
-            <path d="M19.427 21.427a8.5 8.5 0 1 1 2-2l5.585 5.585c.55.55.546 1.43 0 1.976l-.024.024a1.399 1.399 0 0 1-1.976 0l-5.585-5.585zM14.5 21a6.5 6.5 0 1 0 0-13 6.5 6.5 0 0 0 0 13z"
-             fillRule="evenodd"/>
-          </svg>
-        </button>
+
+        <SearchButton style={"search-button"}/>
+
         <input  type="text"
           name="search"
           placeholder="Search..."
